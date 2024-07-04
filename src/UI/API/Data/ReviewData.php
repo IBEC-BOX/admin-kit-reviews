5c94fa338307088719f86468d5dd8601b5b4aa4d
Anastas Mironov <ast@l80.ru>
2024-07-05 02:01:55 +0900

    fix: laravel-data version ^4.5

--- src/UI/API/Data/ReviewData.php
+++ src/UI/API/Data/ReviewData.php
@@ -3,10 +3,13 @@
 namespace AdminKit\Reviews\UI\API\Data;
 
 use AdminKit\Reviews\Models\Review;
+use Spatie\LaravelData\Concerns\WithDeprecatedCollectionMethod;
 use Spatie\LaravelData\Data;
 
 class ReviewData extends Data
 {
+    use WithDeprecatedCollectionMethod;
+
     public function __construct(
         public string $text,
         public string $author,
